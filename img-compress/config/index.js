d657799629a4c647d2b72ef68469428375245fdf
Kane <kiluahaibin@gmail.com>
2019-06-28 19:00:58 +0800

    add webpack

---
+++ img-compress/config/index.js
@@ -0,0 +1,25 @@
+const path = require('path')
+// const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    // 入口文件
+    entry: {
+        entry: path.resolve(__dirname, 'index.js')
+    },
+    // 出口文件的配置
+    output : {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    // 模块
+    module: {
+
+    },
+    // 插件
+    // plugins: [new HtmlWebpackPlugin()],
+    // 配置webpack开发服务功能
+    devServer: {
+        port: 8080,
+        inline: true
+    }
+}
---
+++ img-compress/index.js
@@ -0,0 +1,5 @@
+let imgUtils = require('./imgUtils')
+console.log(imgUtils)
+function testUpload() {
+    let File = document.getElementsByName('testImg')[0].files[0]
+}
